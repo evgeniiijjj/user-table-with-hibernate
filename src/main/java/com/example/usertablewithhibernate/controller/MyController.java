7f83238c4f3a396e341ea1518481503b8d9a653d
evgeniiijjj <evgenijjj78@gmail.com>
2022-11-18 17:53:54 +0300

    add jpa-repository

--- src/main/java/com/example/usertablewithhibernate/controller/MyController.java
+++ src/main/java/com/example/usertablewithhibernate/controller/MyController.java
@@ -1,13 +1,18 @@
 package com.example.usertablewithhibernate.controller;
 
+import com.example.usertablewithhibernate.entity.Person;
 import com.example.usertablewithhibernate.repository.MyRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/persons")
 public class MyController {
     private final MyRepository repository;
 
@@ -15,8 +20,19 @@ public class MyController {
         this.repository = repository;
     }
 
-    @GetMapping("/persons/by-city")
-    public List<String> getProductName(@RequestParam String city) {
-        return repository.getPersonsByCity(city);
+    @GetMapping("/by-city")
+    public List<Person> getPersonsByCity(@RequestParam String city) {
+        return repository.findByCity(city);
+    }
+
+    @GetMapping("/by-age")
+    public List<Person> getPersonsByAge(@RequestParam int age) {
+        return repository.findByIdAgeLessThanOrderByIdAge(age);
+    }
+
+    @GetMapping("/by-name-surname")
+    public Person getPersonByNameAndSurname(@RequestParam String name, @RequestParam String surname) {
+        return repository.findByIdNameAndIdSurname(name, surname)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
--- src/main/java/com/example/usertablewithhibernate/repository/MyRepository.java
+++ src/main/java/com/example/usertablewithhibernate/repository/MyRepository.java
@@ -1,19 +1,16 @@
 package com.example.usertablewithhibernate.repository;
 
+import com.example.usertablewithhibernate.entity.Person;
+import com.example.usertablewithhibernate.entity.PersonId;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public class MyRepository {
-    @PersistenceContext
-    private EntityManager manager;
-
-
-    public List<String> getPersonsByCity(String city) {
-        return manager.createQuery("select p.id.name from Person as p where lower(p.city) = lower(:city)", String.class)
-                .setParameter("city", city).getResultList();
-    }
+public interface MyRepository extends JpaRepository<Person, PersonId> {
+    List<Person> findByCity(String city);
+    List<Person> findByIdAgeLessThanOrderByIdAge(int age);
+    Optional<Person> findByIdNameAndIdSurname(String name, String surname);
 }
