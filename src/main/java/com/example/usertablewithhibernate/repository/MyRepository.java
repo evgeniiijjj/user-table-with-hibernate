17a454e3f37336edad3a0fcc4b3250e74c2c9e9a
evgeniiijjj <evgenijjj78@gmail.com>
2022-11-18 18:08:18 +0300

    add jpa-repository-query

--- src/main/java/com/example/usertablewithhibernate/repository/MyRepository.java
+++ src/main/java/com/example/usertablewithhibernate/repository/MyRepository.java
@@ -3,6 +3,8 @@ package com.example.usertablewithhibernate.repository;
 import com.example.usertablewithhibernate.entity.Person;
 import com.example.usertablewithhibernate.entity.PersonId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,7 +12,10 @@ import java.util.Optional;
 
 @Repository
 public interface MyRepository extends JpaRepository<Person, PersonId> {
-    List<Person> findByCity(String city);
-    List<Person> findByIdAgeLessThanOrderByIdAge(int age);
-    Optional<Person> findByIdNameAndIdSurname(String name, String surname);
+    @Query("select p from Person as p where p.city = :city")
+    List<Person> findByCity(@Param("city") String city);
+    @Query("select p from Person as p where p.id.age < :age order by p.id.age")
+    List<Person> findByIdAgeLessThanOrderByIdAge(@Param("age") int age);
+    @Query("select p from Person as p where p.id.name = :name and p.id.surname = :surname")
+    Optional<Person> findByIdNameAndIdSurname(@Param("name") String name, @Param("surname") String surname);
 }
